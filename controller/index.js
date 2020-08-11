4d20c402e25638977fe94d164d5e25245c6404c5
andres santos <andresrodriguezsantos44@gmail.com>
2020-08-11 13:48:16 -0500

    se realiza un CRUD api rest para la administracion de tareas. la informacion queda guardada en memoria

---
+++ controller/index.js
@@ -0,0 +1,25 @@
+let arrayData = [];
+exports.create = (req) => {
+    req.body.id = arrayData.length + 1;
+    arrayData.push(req.body);
+    return req.body;
+};
+exports.index = () => {
+    return arrayData;
+}
+
+exports.update = (req) => {
+    req.body.id = arrayData[(Number(req.params.id) - 1)].id
+    arrayData[(Number(req.params.id) - 1)] = req.body
+    return arrayData;
+}
+
+exports.delete = (req) => {
+    delete arrayData[req.params.id]
+    return arrayData;
+}
+
+exports.deleteAll = (req) => {
+    arrayData = []
+    return arrayData;
+}
--- router/index.js
+++ router/index.js
@@ -1,25 +1,28 @@
 const express = require('express')
 const router = express.Router();
-
-const arrayData = [];
+const controller = require('../controller');
 
 router.post('/', async (req, res) => {
-    res.status(200).json()
+    res.status(200).json(controller.create(req))
 });
 
 router.post('/', async (req, res) => {
     res.status(200).json()
 });
 
-router.put('/', async (req, res) => {
-    res.status(200).json()
+router.put('/:id', async (req, res) => {
+    res.status(200).json(controller.update(req))
+});
+
+router.delete('/:id', async (req, res) => {
+    res.status(200).json(controller.delete(req))
 });
 
 router.delete('/', async (req, res) => {
-    res.status(200).json()
+    res.status(200).json(controller.deleteAll())
 });
 
 router.get('/', async (req, res) => {
-    res.status(200).json()
+    res.status(200).json(controller.index())
 });
 module.exports = router;
