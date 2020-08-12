00f83da6109a7300a39f984c091710a7c0162daf
andres santos <andresrodriguezsantos44@gmail.com>
2020-08-11 22:46:23 -0500

    upload serve

--- index.js
+++ index.js
@@ -6,14 +6,19 @@ const router = require('./router')
 const Config = require('./config')
 const PORT = process.env.PORT || Config.portWebMain;
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json({type: 'application/x-www-form-urlencoded'}));
 app.use(morgan('dev'));
 app.use('/api', router);
 
+
 try {
-    // init server
     app.listen(PORT, function () {
         console.log(`Server listen in PORT: ${PORT}`);
     });
