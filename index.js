cd62909c199f21f6f9b9d88a6cc07f225d3a19b9
andres santos <andresrodriguezsantos44@gmail.com>
2020-08-12 11:44:49 -0500

    se anexa el cors *

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || Config.portWebMain;
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
     next();
 });
 app.use(bodyParser.json());
